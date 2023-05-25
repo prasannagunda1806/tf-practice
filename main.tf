65708dd9f21fd06387909d3e7b5b3468f4ae070f
prasanna <prasanna.gunda06@gmail.com>
2023-05-25 08:59:21 +0000

    update

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-module "my_instance" {
+module "m-1" {
 
     source = "./m-1"
     
     ami_details = "ami-0b5a2b5b8f2be4ec2"
     
-    instance_type = "t2.micro"
+    instance_type_details = "t2.micro"
     
     vpc_security_group_ids_details =  ["sg-05963ecb7a99cd5e6"]
     
