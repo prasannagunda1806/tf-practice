f83b6485f6258437a06441ba97ab14bfcb43d6d9
prasanna <prasanna.gunda06@gmail.com>
2023-05-25 09:14:17 +0000

    update

--- m-2/main.tf
+++ m-2/main.tf
@@ -1,5 +1,11 @@
+variable "a" {
+    description = "abc "
+}
+
+
+
 output "my_output" {
     description = "passing my ami sg instance_type details to another module for reusaility"
 
-    value = aws_instance.ec2_instance.private_ip
+    value = var.a
 }
